dd3e2e35c3d09b67ba2e98b835bc65cbea186655
Michiii <michael.leisch@gmx.at>
2022-12-02 20:20:47 +0100

    Fixes

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
+
 let newCountryList = countries.sort(() => {return Math.random() - 0.5});
 let index = 0;
 let wrongI = 0;
 let wrongCountrys = new Array();
 let hintCount = 0;
 
-document.querySelector('#content').innerHTML = 
-`<h2>${index+1}/${newCountryList.length}</h2>
-<section>
-<img data-position='show' src="">
-<img data-position='hidden' src="https://flagcdn.com/h120/${newCountryList[index].code.toLowerCase()}.png">
-</section>  
-
-<input id="input" type="text" autocomplete="off" autofocus>`;
-
-let flag = document.querySelector("#input");
-flag.addEventListener("keydown", (event) => {
-    if (event.keyCode == 191) { // # --> Skip
-        event.preventDefault();
-        skip(false, true);
-    }
-    if (event.keyCode == 187){ // * --> Hint
-        event.preventDefault();
-        loadHint();
-    }
-    if (event.keyCode == 13){
-        checkCountry(index);
-    }
-});
-
+loadGame();
 getCountry();
+
+/**
+ * 
+ */
+function loadMenu(){
+
+}
+
+/**
+ * Loads the Game Template into the HTML
+ */
+function loadGame(){
+    document.querySelector('main').innerHTML = 
+        `<div id="content"><h2>${index+1}/${newCountryList.length}</h2>
+        <section>
+            <img data-position='show' src="">
+            <img data-position='hidden' src="https://flagcdn.com/h120/${newCountryList[index].code.toLowerCase()}.png">
+        </section>  
+
+        <input id="input" type="text" autocomplete="off" autofocus></div>
+        <div id="buttons"><i class="fa-solid fa-lightbulb"></i><i class="fa-solid fa-check"></i><i class="fa-solid fa-forward"></i></div>
+        <div id="score"></div>`;
+
+    let flag = document.querySelector("#input");
+    flag.addEventListener("keydown", (event) => {
+        if (event.keyCode == 191) { // # --> Skip
+            event.preventDefault();
+            skip(false, true);
+        }
+        if (event.keyCode == 187){ // * --> Hint
+            event.preventDefault();
+            loadHint();
+        }
+        if (event.keyCode == 13){ // Enter --> Check
+            checkCountry(index);
+        }
+    });
+}
 
 function checkCountry(i){
     let guess = document.querySelector('#input').value.toLowerCase();
@@ -45,7 +61,7 @@ function checkCountry(i){
 
 function loadHint(){
     hintCount++;
-
+    wrongCountrys[wrongI++] = newCountryList[index];
     document.querySelector('#input').value = ""
     document.querySelector('#input').placeholder = newCountryList[index].name[0].substring(0,hintCount)
 
@@ -57,13 +73,16 @@ function skip(ind, type){
     if(!ind){ // Falsche Eingabe
         if(type){ // Geskippte Eingabe
             wrongCountrys[wrongI++] = newCountryList[index];
+            document.querySelector('#input').style.color = "rgb(110, 110, 110)";
             document.querySelector('#input').value = newCountryList[index].name;
             document.querySelector('#input').disabled = true;
             skipped();
 
+
             setTimeout(function(){
                 index++;
                 document.querySelector('#input').disabled = false;
+                document.querySelector('#input').style.color = "white";
                 getCountry();
             },1000);
             return;
@@ -72,9 +91,14 @@ function skip(ind, type){
         // Fail Animation
         document.querySelector('#input').style.color = "red";
         document.querySelector('#input').style.animation = "shake 0.5s"
+        document.querySelectorAll('img')[0].style.color = "red"
+        document.querySelectorAll('img')[0].style.animation = "shake 0.5s 0.2s"
+
         setTimeout(function(){
-            document.querySelector('#input').style.color = "rgb(110, 110, 110)";
+            document.querySelector('#input').style.color = "white";
             document.querySelector('#input').style.animation = "none"
+            document.querySelectorAll('img')[0].style.color = "none"
+            document.querySelectorAll('img')[0].style.animation = "none"
         },500);
         return;
 
@@ -82,8 +106,10 @@ function skip(ind, type){
 
         // Accept Animation
         document.querySelector('#input').style.color = "green";
+        document.querySelector('.fa-check').style.color = "green";
         setTimeout(function(){
-            document.querySelector('#input').style.color = "rgb(110, 110, 110)";
+            document.querySelector('#input').style.color = "white";
+            document.querySelector('.fa-check').style.color = "white";
             index++;
             getCountry();
         },500);
@@ -111,8 +137,7 @@ function getCountry() {
 
     hintCount = 0;
     
-    document.querySelector('#buttons').innerHTML = `<p onclick="loadHint()">Tipp (+)</p><p onclick="skip()">Weiter (#)</p>`
-    document.querySelector('#hint').innerHTML = `Richtig ${index - wrongI} / ${index}`;
+    document.querySelector('#score').innerHTML = `Richtig ${index - wrongI} / ${index}`;
     let flag = document.querySelector("#input");
     flag.focus();
 
