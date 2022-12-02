33b523edc4d8100e366b926ff092128524a2e974
Michael Leisch <92372034+Michiii11@users.noreply.github.com>
2022-12-02 14:47:42 +0100

    animation

--- script.js
+++ script.js
@@ -4,6 +4,30 @@ let wrongI = 0;
 let wrongCountrys = new Array();
 let hintCount = 0;
 
+document.querySelector('#content').innerHTML = 
+`<h2>${index+1}/${newCountryList.length}</h2>
+<section>
+<img data-position='show' src="">
+<img data-position='hidden' src="https://flagcdn.com/h120/${newCountryList[index].code.toLowerCase()}.png">
+</section>  
+
+<input id="input" type="text" autocomplete="off" autofocus>`;
+
+let flag = document.querySelector("#input");
+flag.addEventListener("keyup", (event) => {
+    if (event.keyCode == 191) { // # --> Skip
+        event.preventDefault();
+        skip(false, true);
+    }
+    if (event.keyCode == 187){ // * --> Hint
+        event.preventDefault();
+        loadHint();
+    }
+    if (event.keyCode == 13){
+        checkCountry(index);
+    }
+});
+
 getCountry();
 
 function checkCountry(i){
@@ -22,6 +46,7 @@ function checkCountry(i){
 function loadHint(){
     hintCount++;
 
+    document.querySelector('#input').value = ""
     document.querySelector('#input').placeholder = newCountryList[index].name[0].substring(0,hintCount)
 
     let flag = document.querySelector("#input");
@@ -29,31 +54,47 @@ function loadHint(){
 }
 
 function skip(ind, type){
-    if(!ind){
-        if(type){
+    if(!ind){ // Falsche Eingabe
+        if(type){ // Geskippte Eingabe
             wrongCountrys[wrongI++] = newCountryList[index];
+            document.querySelector('#input').value = newCountryList[index].name;
             document.querySelector('#input').disabled = true;
             skipped();
 
             setTimeout(function(){
-                index++
+                index++;
                 document.querySelector('#input').disabled = false;
                 getCountry();
-            },2000);
+            },1000);
             return;
         }
 
+        // Fail Animation
         document.querySelector('#input').style.color = "red";
         document.querySelector('#input').style.animation = "shake 0.5s"
-
         setTimeout(function(){
             document.querySelector('#input').style.color = "rgb(110, 110, 110)";
             document.querySelector('#input').style.animation = "none"
         },500);
         return;
-    } 
-    index++;
-    getCountry();
+
+    } else{ // Richtige Eingabe
+
+        // Accept Animation
+        document.querySelector('#input').style.color = "green";
+        setTimeout(function(){
+            document.querySelector('#input').style.color = "rgb(110, 110, 110)";
+            index++;
+            getCountry();
+        },500);
+    }
+}
+
+function swap(){
+    let hid = document.querySelector('#content img[data-position="hidden"]')
+    let show = document.querySelector('#content img[data-position="show"]')
+    hid.setAttribute("data-position", "show");
+    show.setAttribute("data-position", "hidden");
 }
 
 function skipped(){
@@ -61,30 +102,17 @@ function skipped(){
 }
 
 function getCountry() {
-    hintCount = 0;
-    document.querySelector('#content').innerHTML = 
-    `<h2>${index+1}/${newCountryList.length}</h2>
-    <img src="https://flagcdn.com/h120/${newCountryList[index].code.toLowerCase()}.png">
-    <input id="input" type="text" autocomplete="off" autofocus>`;
-
+    document.querySelector('#content img[data-position="hidden"]').setAttribute("src", `https://flagcdn.com/h120/${newCountryList[index].code.toLowerCase()}.png`);
+    document.querySelector('#input').value = "";
+    document.querySelector('#input').placeholder = "";
     console.log(newCountryList[index].name)
+    swap();
+
+    hintCount = 0;
     
-    document.querySelector('#buttons').innerHTML = `<p onclick="loadHint()">Tipp</p><p onclick="skip()">Weiter</p>`
+    document.querySelector('#buttons').innerHTML = `<p onclick="loadHint()">Tipp (+)</p><p onclick="skip()">Weiter (#)</p>`
     document.querySelector('#hint').innerHTML = `Richtig ${index - wrongI} / ${index}`;
     let flag = document.querySelector("#input");
     flag.focus();
 
-    flag.addEventListener("keydown", (event) => {
-        if (event.keyCode == 191) {
-            event.preventDefault();
-            skip(false, true);
-        }
-        if (event.keyCode == 187){
-            event.preventDefault();
-            loadHint();
-        }
-        if (event.keyCode == 13){
-            checkCountry(index);
-        }
-    });
 }
