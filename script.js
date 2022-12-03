013fd8e2ccaf77b90bb5f41bcebea93f66fbedf3
Michiii <michael.leisch@gmx.at>
2022-12-03 18:46:20 +0100

    Home 1

--- script.js
+++ script.js
@@ -5,30 +5,50 @@ let wrongI = 0;
 let wrongCountrys = new Array();
 let hintCount = 0;
 
-loadGame();
-getCountry();
+let type = "H"  // L = Country | H = Capital | C = Competitive
+
+loadMenu();
 
 /**
- * 
+ * Loads the Menu into the HTML
  */
 function loadMenu(){
-
+    document.querySelector('main').innerHTML =
+        `<div id="content">
+            <h2>Menü</h2>
+            <div class="buttons">
+                <p onclick="loadGame()">Country</p>
+                <p onclick="loadGame(true)">Capital</p>
+                <p onclick="loadGame()">Comp</p>
+            </div>
+        </div>`
 }
 
 /**
  * Loads the Game Template into the HTML
+ * @param {*} type is the Gamemode
  */
-function loadGame(){
-    document.querySelector('main').innerHTML = 
+function loadGame(type){
+    let content = ""
+    content = 
         `<div id="content"><h2>${index+1}/${newCountryList.length}</h2>
         <section>
             <img data-position='show' src="">
             <img data-position='hidden' src="https://flagcdn.com/h120/${newCountryList[index].code.toLowerCase()}.png">
-        </section>  
+        </section>`
 
+    if(type){
+        console.log()
+        content += `<h3></h3>`
+    }
+
+    content += `
         <input id="input" type="text" autocomplete="off" autofocus></div>
-        <div id="buttons"><i class="fa-solid fa-lightbulb"></i><i class="fa-solid fa-check"></i><i class="fa-solid fa-forward"></i></div>
-        <div id="score"></div>`;
+        <div class="buttons"><i class="fa-solid fa-lightbulb"></i><i class="fa-solid fa-check"></i><i class="fa-solid fa-forward"></i></div>
+        <div id="score"></div>
+        <div id="back" onclick="loadMenu()"><i class="fa-solid fa-house"></i></div>`;
+
+    document.querySelector('main').innerHTML = content;
 
     let flag = document.querySelector("#input");
     flag.addEventListener("keydown", (event) => {
@@ -44,11 +64,21 @@ function loadGame(){
             checkCountry(index);
         }
     });
+    getCountry();
 }
 
+/**
+ * Checks if the guess is correct
+ * if it's true it goes into skip()
+ * @param {*} i 
+ */
 function checkCountry(i){
     let guess = document.querySelector('#input').value.toLowerCase();
+    
     let answer = newCountryList[index].name;
+    if(type == "H"){
+        answer = newCountryList[index].capital;
+    }
 
     for (let i = 0; i < answer.length; i++) {
         if(guess.replace(" ", "") == answer[i].toLowerCase().replace(" ", "")){
@@ -59,13 +89,20 @@ function checkCountry(i){
     skip(false);
 }
 
+/**
+ * Loads an hint for the flag
+ */
 function loadHint(){
     hintCount++;
     if(hintCount == 1){
         wrongCountrys[wrongI++] = newCountryList[index];
     }
     document.querySelector('#input').value = ""
-    document.querySelector('#input').placeholder = newCountryList[index].name[0].substring(0,hintCount)
+    if(type == "H"){
+        document.querySelector('#input').placeholder = newCountryList[index].capital[0].substring(0,hintCount)
+    } else{
+        document.querySelector('#input').placeholder = newCountryList[index].name[0].substring(0,hintCount)
+    }
 
     let flag = document.querySelector("#input");
     flag.focus();
@@ -76,7 +113,13 @@ function skip(ind, type){
         if(type){ // Geskippte Eingabe
             wrongCountrys[wrongI++] = newCountryList[index];
             document.querySelector('#input').style.color = "rgb(110, 110, 110)";
-            document.querySelector('#input').value = newCountryList[index].name[0];
+            
+            if(type == "H"){
+                document.querySelector('#input').value = newCountryList[index].capital[0];
+            } else{
+                document.querySelector('#input').value = newCountryList[index].name[0];
+            }
+
             document.querySelector('#input').disabled = true;
             skipped();
 
@@ -119,8 +162,12 @@ function swap(){
     show.setAttribute("data-position", "hidden");
 }
 
-function skipped(){
-    document.querySelector('#input').placeholder = newCountryList[index].name[0];
+function skipped(){    
+    if(type == "H"){
+        document.querySelector('#input').placeholder = newCountryList[index].capital[0];
+    } else{
+        document.querySelector('#input').placeholder = newCountryList[index].name[0];
+    }
 }
 
 function getCountry() {
@@ -128,7 +175,13 @@ function getCountry() {
     document.querySelector('#content img[data-position="hidden"]').setAttribute("src", `https://flagcdn.com/h120/${newCountryList[index].code.toLowerCase()}.png`);
     document.querySelector('#input').value = "";
     document.querySelector('#input').placeholder = "";
-    console.log(newCountryList[index].name)
+    if(type == "H"){
+        document.querySelectorAll('#content h3')[0].innerHTML = newCountryList[index].name[0]
+        console.log(newCountryList[index].capital)
+    } else{
+        console.log(newCountryList[index].name)
+    }
+
     swap();
 
     hintCount = 0;
