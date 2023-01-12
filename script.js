5f3a71187a005111399e077442ba5b1fd43aac85
Michael Leisch <92372034+Michiii11@users.noreply.github.com>
2023-01-12 17:07:10 +0100

    bug fix & capital city
    local storage bug fix
    capital city flag show and hide

--- script.js
+++ script.js
@@ -8,19 +8,11 @@ let hintCount = 0; // Count of the shown letters
 let wrongCountrys = []; // List of the wrong anwsers
 
 let selectorOrder = {
-    "flagType": "country",
-    "flagContinent": "all",
-    "flagMode": "unranked"
+    flagType: "country",
+    flagContinent: "all",
+    flagStyle: "show"
 }
-// Country | Capital // Current continent // Unranked | Ranked
-
-
-let points // Ranked current points
-let tryCount // Ranked try count
-let pointsPerTry = [1200, 800, 600, 400, 200] // Point matrix per try
-let lives
-let startTime
-let endTime
+// Country | Capital // Current continent
 
 const completeCounter = document.querySelectorAll('#game h2')[0]; // e.g. 0/5
 
@@ -41,12 +33,24 @@ const inputField = document.querySelector('#input'); // input Field of the page
 //#region
 
 // Set or load the Localstorage
-for (let key in selectorOrder){
+console.log(selectorOrder)
+
+for (const [key, value] of Object.entries(selectorOrder)) {
+    console.log(`${key}: ${value}`);
+
     if (localStorage.getItem(key) != undefined) {
-        selectorOrder.key = localStorage.getItem(key)
+        selectorOrder[key] = localStorage.getItem(key)
     } else {
-        localStorage.setItem(key, selectorOrder.key);
+        localStorage.setItem(key, value)
     }
+}
+
+document.querySelector('.type .selected').classList.remove("selected")
+document.querySelector('.continent .selected').classList.remove("selected")
+document.querySelector(`.${selectorOrder.flagType}`).classList.add('selected')
+document.querySelector(`.${selectorOrder.flagContinent}`).classList.add('selected')
+if(selectorOrder.flagStyle == "show"){
+    hideShowFlag();
 }
 
 // Pages
@@ -78,9 +82,6 @@ function loadSide(t) {
             break;
         case "G":
             gamePage.style.display = "block";
-            if (selectorOrder.flagMode == "ranked") {
-                startRankedGame()
-            }
             startGame();
             break;
         case "F":
@@ -94,11 +95,13 @@ function startGame() {
     // Set start img
     showBox().innerHTML = `<img src="">`
     hidBox().innerHTML = `<img src="https://flagcdn.com/h120/${countryList[index].code.toLowerCase()}.png">`
+    document.querySelector('.hideButton').style.display = "none"
 
     // Capital Mode 
     if (selectorOrder.flagType == "capital") {
         showBox().innerHTML += `<h3></h3>`
         hidBox().innerHTML += `<h3>${countryList[index].name[0]}</h3>`
+        document.querySelector('.hideButton').style.display = "block"
     }
 
     getCountry();
@@ -142,7 +145,7 @@ function selector(elem, type) {
         elem.classList.add("selected")
         selectorOrder.flagType = elem.classList[0]
 
-        localStorage.setItem('selectorOrder.flagType', selectorOrder.flagType);
+        localStorage.setItem('flagType', selectorOrder.flagType);
         setCountryList();
     }
 
@@ -151,16 +154,7 @@ function selector(elem, type) {
         elem.classList.add("selected")
         selectorOrder.flagContinent = elem.classList[0]
 
-        localStorage.setItem('selectorOrder.flagContinent', selectorOrder.flagContinent);
-        setCountryList();
-    }
-
-    if (type == "M") {
-        document.querySelector('.mode .selected').classList.remove("selected")
-        elem.classList.add("selected")
-        selectorOrder.flagMode = elem.classList[0]
-
-        localStorage.setItem('selectorOrder.flagMode', selectorOrder.flagMode);
+        localStorage.setItem('flagContinent', selectorOrder.flagContinent);
         setCountryList();
     }
 }
@@ -180,7 +174,6 @@ function setCountryList(isNewRound) {
             countryList = [...countries];
         } else {
             for (let i = 0; i < countries.length; i++) {
-                console.log(selectorOrder)
                 if (countries[i].continent == selectorOrder.flagContinent) { // Filter the continent
                     countryList.push(countries[i]); // Set the country into the country list
                 }
@@ -196,57 +189,6 @@ function setCountryList(isNewRound) {
     // Reset the 
     index = 0;
     wrongCountrys = [];
-}
-
-/**
- * Starts a ranked game
- */
-function startRankedGame() {
-    points = 0;
-    tryCount = 0;
-    lives = 3;
-
-    let continents = [
-        ["Europa", 1/* 7 */], 
-        ["Afrika", 1/* 10 */],
-        ["Asien", 1/* 9 */],
-        ["Nordamerika", 1/* 2 */],
-        ["Südamerika", 1/* 1 */],
-        ["Ozeanien", 1/* 1 */]
-    ]
-
-    let tempList = [];
-    countryList = [];
-
-    // Get Continents seperate
-    for (let i = 0; i < continents.length; i++) {
-        tempList[i] = [];
-        for (let j = 0; j < countries.length; j++) {
-            if (countries[j].continent == continents[i][0]) {
-                tempList[i].push(countries[j]);
-            }
-        }
-        tempList[i] = tempList[i].sort(() => {
-            return Math.random() - 0.5
-        })
-    }
-
-    // Push specifiy amount of countries per continent
-    for (let i = 0; i < continents.length; i++) {
-        for (let j = 0; j < continents[i][1]; j++) {
-            countryList.push(tempList[i][j])
-        }
-    }
-    
-    // Shuffle list
-    countryList = countryList.sort(() => {
-        return Math.random() - 0.5
-    })
-
-    document.querySelector('#game .buttons').innerHTML =
-    `<i title="Enter" class="fa-solid fa-check" onclick="checkCountry()"></i>
-    <a></a>
-    <i title="#" class="fa-solid fa-forward" onclick="skip()"></i></div>`
 }
 
 //#endregion
@@ -283,24 +225,7 @@ function getCountry() {
 
     hintCount = 0; // New country = reset hintCount
 
-    if(selectorOrder.flagMode == "unranked"){
-        scoreField.innerHTML = `Richtig ${index - wrongCountrys.length} / ${index}`;
-    } else{
-        tryCount = 0;
-        document.querySelector('#game .buttons a').innerHTML = points;
-        startTime = Math.floor(Date.now() / 1000);
-        document.querySelector('.load').style.display = "block"
-
-        scoreField.innerHTML = ""
-        for (let i = 0; i < 3; i++) {
-            if(i < lives){
-                scoreField.innerHTML += `<i class="fa-solid fa-heart"></i>`
-            } else{
-                scoreField.innerHTML += `<i class="fa-regular fa-heart"></i>`
-            }
-        }
-
-    }
+    scoreField.innerHTML = `Richtig ${index - wrongCountrys.length} / ${index}`;
     inputField.focus();
 }
 
@@ -321,28 +246,11 @@ function checkCountry() {
     // Loop goes throw all names from current Country
     for (let i = 0; i < answer.length; i++) {
         if (guess.replace(" ", "") == answer[i].toLowerCase().replace(" ", "")) {
-            if(selectorOrder.flagMode == "ranked"){
-                if(tryCount < pointsPerTry.length){
-                    document.querySelector('.load').style.display = "none"
-    
-                    endTime = Math.floor(Date.now() / 1000);
-                    let timeDiff = Math.ceil(endTime-startTime);
-    
-                    let multiplier = 1 / (Math.ceil(timeDiff) / 3);
-                    points += pointsPerTry[tryCount]*multiplier;
-                } else{
-                    tryCount = 0;
-                    skip(false);
-                    return;
-                }
-            }
-
             skip(true);
             return;
         }
     }
 
-    tryCount ++;
     skip(false);
 }
 
@@ -455,7 +363,6 @@ function skipped() {
     } else {
         inputField.placeholder = countryList[index].name[0];
     }
-    lives--;
 }
 
 function finishedRound() {
@@ -470,6 +377,21 @@ function finishedRound() {
             `<p onclick="setCountryList(true);loadSide('G');">Weiter</p>
         <p onclick="loadSide('M')">Home</p>`
     }
+}
+
+function hideShowFlag(){
+    document.querySelectorAll('#content').forEach((elem)=>{
+        elem.classList.toggle("big")
+
+        if(elem.classList.contains("big")){
+            selectorOrder.flagStyle = "show"
+        } else{
+            selectorOrder.flagStyle = "hidden"
+        }
+
+        localStorage.setItem("flagStyle", selectorOrder.flagStyle)
+    })
+    inputField.focus();
 }
 
 //#endregion
